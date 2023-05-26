17381b0f700d36817bc88179d873c3b15098fbd4
zhangyiyou <zhangyiyou5@gmail.com>
2023-05-26 15:50:01 +0800

    edit key

--- src/app.js
+++ src/app.js
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
       .then(() => navigator.serviceWorker.ready)
       .then(serviceWorkerRegistration =>
         serviceWorkerRegistration.pushManager.subscribe({
-          userVisibleOnly: true,
+          userVisibleOnly: false,
           applicationServerKey: urlBase64ToUint8Array(applicationServerKey),
         })
       )

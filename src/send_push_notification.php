28d0d903e9950c1eb7d36cbd842a5dd68aac90bc
zhangyiyou <zhangyiyou5@gmail.com>
2023-05-26 15:20:02 +0800

    edit key

--- src/send_push_notification.php
+++ src/send_push_notification.php
@@ -9,14 +9,13 @@ use Minishlink\WebPush\VAPID;
 // because you already stored it (cf. push_subscription.php)
 $subscription = Subscription::create(json_decode(file_get_contents('php://input'), true));
 
-var_dump(VAPID::createVapidKeys());
+//var_dump(VAPID::createVapidKeys());
 
 $auth = array(
     'VAPID' => array(
         'subject' => 'https://webpush.powerbuyin.top/',
         'publicKey' => file_get_contents(__DIR__ . '/../keys/public_key.txt'), // don't forget that your public key also lives in app.js
         'privateKey' => file_get_contents(__DIR__ . '/../keys/private_key.txt'), // in the real world, this would be in a secret file
-        'pemFile' => file_get_contents(__DIR__ . '/../keys/private_key.pem'),
     ),
 );
 

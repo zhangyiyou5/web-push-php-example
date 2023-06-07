0bfa2a2e584aca53929328dc85a44be99113b1f8
T7n69vV6sZsxdJVU <249340024@qq.com>
2023-06-07 13:23:50 +0800

    111

--- src/send_push_notification.php
+++ src/send_push_notification.php
@@ -5,8 +5,8 @@ use Minishlink\WebPush\Subscription;
 use Minishlink\WebPush\VAPID;
 
 
-$subscription = Subscription::create(json_decode(file_get_contents('php://input'), true));
-var_dump($subscription);
+//$subscription = Subscription::create(json_decode(file_get_contents('php://input'), true));
+//var_dump($subscription);
 
 $auth = array(
     'VAPID' => array(
@@ -19,13 +19,13 @@ $auth = array(
 $webPush = new WebPush($auth);
 
 //参数从数据库获取
-//$subscription = Subscription::create([
-//    'endpoint' => 'https://fcm.googleapis.com/fcm/send/d0TH_3q920U:APA91bEXaWgUZx7VV7SZKM4XUua_sTp9FrY6uhhPUgOoCiQ_zfyCP2IlhGcV0cODj06e5i0BP599Yb9m7gkxEYJCh9TPvqd5Wbv1Ad1qjdvwf4bf96FwgwRY5cb9s5eLjwnLERItMRs0', // Firefox 43+,
-//    'publicKey' => 'BMBsQg3PP56yG6eqIAIf5rfrwKxGsRbwvy0fG1cUTyhvHWl+4QMI260FO72z4MkH8m+iV9oyK+O8x+bIWSGowl0=', // base 64 encoded, should be 88 chars
-//    'authToken' => 'nCcpo8Bg8fLVVFtRs88GuA==', // base 64 encoded, should be 24 chars
-//]);
+$subscription = Subscription::create([
+    'endpoint' => 'https://fcm.googleapis.com/fcm/send/d06cWYtZLL0:APA91bH9EljjzSwKylSdwz4F5VPVw-JwHFB9KpP-pV9TD419tKlpr1lRx-ZoqqmqA3pYsen8YX7KE7Qxc_iT8_kehTO2GrJNqCNaVEl12IYh7euNAW9lvxv_-2maYrxJNUYXhXGduab7', // Firefox 43+,
+    'publicKey' => 'BP+/KZygGBY11YEjZzY4uvNMfbG8KCzB+XiQLLLEaRmelJHqmEMd49l250M5+LVqI6zSmUaNqVVMrn7qs9b5JRY=', // base 64 encoded, should be 88 chars
+    'authToken' => 'weVd2PFvuwGhRgiNaFBarA==', // base 64 encoded, should be 24 chars
+]);
 
-//var_dump($subscription);
+var_dump($subscription);
 
 try {
     $report = $webPush->sendOneNotification(
